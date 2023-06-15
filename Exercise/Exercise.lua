b4d7872f2488cb2d7251f7c4dba0bb1e1dc8706d
Elias Howell <eliasjameshowell@icloud.com>
2023-06-15 17:24:44 -0400

    keep count of exercises, remove window, use chat

--- Exercise/Exercise.lua
+++ Exercise/Exercise.lua
@@ -1,84 +1,70 @@
--- Define exercises
-local exercises = {
-    "Do 20 push-ups!",
-    "Plank for 60 seconds!",
-    "Do 20 squats!",
-    "Do 5 chin-ups!",
-    "Do 5 pull-ups!",
-}
-
--- Create frame and register events
+-- Create a frame to handle events
 local frame = CreateFrame("Frame")
+
+-- Register event handlers for quest completion and player leveling up
 frame:RegisterEvent("QUEST_TURNED_IN")
 frame:RegisterEvent("PLAYER_LEVEL_UP")
-frame:RegisterEvent("ADDON_LOADED")
 
--- Initialize exercise list
-ExerciseList = ExerciseList or {}
+-- Define the possible exercises
+local exercises = {
+    "20 pushups",
+    "60 seconds plank",
+    "20 squats",
+    "5 chin-ups",
+    "5 pull-ups"
+}
 
--- Create UI Frame
-local uiFrame = CreateFrame("Frame", "ExerciseAddonFrame", UIParent, "BasicFrameTemplateWithInset")
-uiFrame:SetSize(200, 300)
-uiFrame:SetPoint("CENTER")
-uiFrame.title = uiFrame:CreateFontString(nil, "OVERLAY")
-uiFrame.title:SetFontObject("GameFontHighlight")
-uiFrame.title:SetPoint("LEFT", uiFrame.TitleBg, "LEFT", 5, 0)
-uiFrame.title:SetText("Exercise List")
-uiFrame.scrollFrame = CreateFrame("ScrollFrame", nil, uiFrame, "UIPanelScrollFrameTemplate")
-uiFrame.scrollFrame:SetSize(150, 200)
-uiFrame.scrollFrame:SetPoint("TOPLEFT", uiFrame, "TOPLEFT", 20, -30)
-uiFrame.scrollChild = CreateFrame("Frame")
-uiFrame.scrollChild:SetSize(150, 200)
-uiFrame.scrollFrame:SetScrollChild(uiFrame.scrollChild)
-uiFrame.scrollFrame.ScrollBar:ClearAllPoints()
-uiFrame.scrollFrame.ScrollBar:SetPoint("TOPLEFT", uiFrame.scrollFrame, "TOPRIGHT", -12, -18)
-uiFrame.scrollFrame.ScrollBar:SetPoint("BOTTOMRIGHT", uiFrame.scrollFrame, "BOTTOMRIGHT", -7, 18)
-uiFrame:Hide()
+-- Initialize a count of each exercise
+local exercise_count = {
+    pushups = 0,
+    plank = 0,
+    squats = 0,
+    chin_ups = 0,
+    pull_ups = 0
+}
 
--- Event handler
-frame:SetScript("OnEvent", function(self, event, ...)
-    if event == "ADDON_LOADED" then
-        local name = ...
-        if name == "ExerciseAddon" then
-            -- Restore saved exercise list, or default to empty list
-            ExerciseList = ExerciseList or {}
-        end
-    elseif event == "QUEST_TURNED_IN" or event == "PLAYER_LEVEL_UP" then
-        -- Randomly select exercise and add to list
-        local exercise = exercises[math.random(#exercises)]
-        table.insert(ExerciseList, exercise)
-
-        -- Display message
-        print("|cffffff00Exercise time! (" .. #ExerciseList .. " pending) " .. exercise .. "|r")
+-- Define event handler function
+frame:SetScript("OnEvent", function(self, event)
+    -- Select a random exercise
+    local random_exercise = exercises[math.random(#exercises)]
+    
+    -- Update the count for the selected exercise
+    if random_exercise == "20 pushups" then
+        exercise_count.pushups = exercise_count.pushups + 20
+    elseif random_exercise == "60 seconds plank" then
+        exercise_count.plank = exercise_count.plank + 60
+    elseif random_exercise == "20 squats" then
+        exercise_count.squats = exercise_count.squats + 20
+    elseif random_exercise == "5 chin-ups" then
+        exercise_count.chin_ups = exercise_count.chin_ups + 5
+    elseif random_exercise == "5 pull-ups" then
+        exercise_count.pull_ups = exercise_count.pull_ups + 5
     end
 end)
 
--- Create slash command to reset list
-SLASH_EXERCISED1 = "/exercised"
-SlashCmdList["EXERCISED"] = function(msg)
-    -- Reset exercise list
-    ExerciseList = {}
-    print("|cffffff00Exercise list reset!|r")
-end
-
--- Create slash command to show exercise list
+-- Define slash command to show current exercises
 SLASH_EXERCISE1 = "/exercise"
 SlashCmdList["EXERCISE"] = function(msg)
-    -- Clear old data
-    for i = uiFrame.scrollChild:GetNumChildren(), 1, -1 do
-        local child = select(i, uiFrame.scrollChild:GetChildren())
-        child:Hide()
-    end
-
-    -- Create text fields for each exercise
-    for i, exercise in ipairs(ExerciseList) do
-        local textField = uiFrame.scrollChild:CreateFontString(nil, "OVERLAY")
-        textField:SetFontObject("GameFontHighlight")
-        textField:SetPoint("TOPLEFT", uiFrame.scrollChild, "TOPLEFT", 0, -(i-1)*15)
-        textField:SetText(exercise)
-    end
-
-    -- Show frame
-    uiFrame:Show()
+    -- Print current exercises in chat
+    DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Push-ups: " .. exercise_count.pushups .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Plank (seconds): " .. exercise_count.plank .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Squats: " .. exercise_count.squats .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Chin-ups: " .. exercise_count.chin_ups .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Pull-ups: " .. exercise_count.pull_ups .. "|r")
 end
 
+-- Define slash command to reset exercises
+SLASH_EXERCISED1 = "/exercised"
+SlashCmdList["EXERCISED"] = function(msg)
+    -- Reset exercises
+    exercise_count = {
+        pushups = 0,
+        plank = 0,
+        squats = 0,
+        chin_ups = 0,
+        pull_ups = 0
+    }
+    
+    -- Print message indicating exercises have been reset
+    DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Exercises reset!|r")
+end
